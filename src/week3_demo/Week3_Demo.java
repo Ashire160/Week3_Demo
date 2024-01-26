b899b567f9023165f28be353073041324a49eada
Ashire160 <157149694+Ashire160@users.noreply.github.com>
2024-01-26 12:41:00 -0500

    Update Week3_Demo.java

--- src/week3_demo/Week3_Demo.java
+++ src/week3_demo/Week3_Demo.java
@@ -16,6 +16,7 @@ public class Week3_Demo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Change");
+        //Trying Fetch and Merge
     }
     
 }
